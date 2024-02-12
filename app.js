e7615418f46d5790794510172a889e796dc8a640
OsvaldoTorre <allone2911@gmail.com>
2024-02-11 23:17:22 -0600

    test: routes

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const db = require('./models/db')
+const controller = require('./controllers/clienteController')
+const clienteRouter  = require('./routes/clienteRouter')
 
 const app = express()
 const port = 3000
 
 app.use(express.static('public'));
 
-app.get('/22', (req, res) => {
-  res.send('holaeeee')
-})
+app.use(express.json()); // para poder parsear JSON
+app.use('/cliente', clienteRouter);
 
 app.listen(port, () => {
   console.log(`\x1b[32m%s\x1b[0m`, `Aplicación ejecutándose en http://localhost:${port}`);
