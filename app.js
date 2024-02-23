b38d0bb3bda738b25ee1b326c9d14e878ca070f7
OsvaldoTorre <allone2911@gmail.com>
2024-02-22 23:30:30 -0600

    oauth

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const db = require('./models/db')
+import passport from 'passport';
+import { loginRouter } from './routes/login.js';
 const controller = require('./controllers/clienteController')
 const clienteRouter  = require('./routes/clienteRouter')
 
@@ -8,8 +10,12 @@ const port = 3000
 
 app.use(express.static('public'));
 
+//MIDDLEWARES
 app.use(express.json()); // para poder parsear JSON
 app.use('/cliente', clienteRouter);
+app.use(passport.initialize());
+
+app.use("/auth", loginRouter)
 
 app.listen(port, () => {
   console.log(`\x1b[32m%s\x1b[0m`, `Aplicación ejecutándose en http://localhost:${port}`);
